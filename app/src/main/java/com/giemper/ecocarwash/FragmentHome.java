8c69d74d625ddb6bd0334cb4461308eea72ce9a4
Guillermo Magdaleno <gmpco51@gmail.com>
2018-02-05 22:43:55 -0800

    02/05

---
+++ app/src/main/java/com/giemper/ecocarwash/FragmentHome.java
@@ -0,0 +1,67 @@
+package com.giemper.ecocarwash;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by gmoma on 2/5/2018.
+ */
+
+public class FragmentHome extends FragmentPagerAdapter {
+
+    public Context mContext;
+    public FragmentHomeTimer Home_Timer;
+    public FragmentHomePeople Home_People;
+    public FragmentHomeRegistry Home_Registry;
+
+    public FragmentHome(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+
+        Home_Timer = new FragmentHomeTimer();
+        Home_People = new FragmentHomePeople();
+        Home_Registry = new FragmentHomeRegistry();
+    }
+
+    // This determines the fragment for each tab
+    @Override
+    public Fragment getItem(int position)
+    {
+        if (position == 0) {
+            return Home_Timer;
+        } else if (position == 1){
+            return Home_People;
+        } else {
+            return Home_Registry;
+        }
+    }
+
+    // This determines the number of tabs
+    @Override
+    public int getCount()
+    {
+        return 3;
+    }
+
+    // This determines the title for each tab
+    @Override
+    public CharSequence getPageTitle(int position)
+    {
+        // Generate title based on item position
+
+        switch (position) {
+            case 0:
+                return mContext.getString(R.string.Home_Tab1);
+            case 1:
+                return mContext.getString(R.string.Home_Tab2);
+            case 2:
+                return mContext.getString(R.string.Home_Tab3);
+            default:
+                return null;
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/giemper/ecocarwash/FragmentHomePeople.java
@@ -0,0 +1,29 @@
+package com.giemper.ecocarwash;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentHomePeople extends Fragment {
+
+
+    public FragmentHomePeople() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home_people, container, false);
+    }
+
+}
